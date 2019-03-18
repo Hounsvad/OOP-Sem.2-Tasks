38749aa6b9ff7b3141b202984a46299958f0813a
Frederik Hounsvad <Frederik@hounsvad.dk>
2019-03-18 09:01:58 +0100

    Garbage

--- src/lesson5/thread_excercise/SocketClient.java
+++ src/lesson5/thread_excercise/SocketClient.java
@@ -2,6 +2,7 @@
 import java.util.Scanner;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Date;
 
 public class SocketClient {
 
@@ -13,13 +14,16 @@ public class SocketClient {
         try (Socket clientSocket = new Socket("localhost", 3333);
                 Scanner keyboard = new Scanner(System.in)) {
             // Set up streams to send/receive data
-            inputStream = new Scanner(clientSocket.getInputStream());
             outputStream = new PrintWriter(clientSocket.getOutputStream(), true);
+            inputStream = new Scanner(clientSocket.getInputStream());
+            
 
             // Start massage from server:
+            System.out.println("Getting line");
             System.out.println(inputStream.nextLine());
+            System.out.println("Line gotten");
             // Read a line from the keyboard:
-            outputStream.println(keyboard.nextLine());
+            outputStream.println("Hello this is string" + new Date().toString() + "\n");
 
             // Read answer from the server and output it to the screen
             str = inputStream.nextLine();
